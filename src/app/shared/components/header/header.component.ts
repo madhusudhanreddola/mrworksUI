53f848c7afdd1d2d582bc994b7ebfd54b467eda8
Reddola <madhusudhan.reddola@gta.ga.gov>
2021-12-13 22:32:58 -0500

    responsive layout

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  @Input() deviceLayout:string = '';
+
+  @Output() toggleSideBarForMe: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log('Header deviceLayout : ' + this.deviceLayout);
+  }
+
+  toggleSideBar(){
+    this.toggleSideBarForMe.emit();
+    setTimeout(() => {
+      window.dispatchEvent(
+        new Event('resize')
+      );
+    }, 300);
   }
 
 }
